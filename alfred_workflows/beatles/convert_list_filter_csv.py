34941fe9c7e5f27aa991c27637905322334f5fe4
Reorx <novoreorx@gmail.com>
2018-06-27 22:02:38 +0800

    generate beatles searcher

--- alfred_workflows/beatles/convert_list_filter_csv.py
+++ alfred_workflows/beatles/convert_list_filter_csv.py
@@ -2,6 +2,7 @@
 
 import re
 import csv
+import json
 
 
 class K:
@@ -69,12 +70,13 @@ def trim_braces(s):
 
 
 def main():
-    songs = []
+    sd_list = []
     rows = []
     with open('./beatles_songs.csv', 'r') as fi:
         r = csv.DictReader(fi)
         for i in r:
             sd = to_song_dict(i)
+            sd_list.append(sd)
             rows.append(to_alfred_dict(sd))
 
     with open('./list_filter.csv', 'w') as fo:
@@ -83,6 +85,73 @@ def main():
         for row in rows:
             w.writerow(row)
 
+    with open('./beatles_searcher.py', 'w') as fpy:
+        songs_def = '{\n'
+        for sd in sd_list:
+            # keep only letter
+            signature = ''.join(i for i in sd['name'] if i.isalpha()).lower()
+            songs_def += '"{}": {},\n'.format(signature, json.dumps(sd, ensure_ascii=False))
+        songs_def += '}'
+        code = code_tmpl.format(songs_def)
+        fpy.write(code)
+
+
+code_tmpl = """\
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+from difflib import SequenceMatcher
+
+songs = {}
+
+re_brackets = re.compile(r'\([^()]+\)')
+
+DEBUG = os.environ.get('BS_DEBUG', False)
+MATCH_MODE = os.environ.get('BS_MATCH_MODE', 'precise')
+
+def debugp(s):
+    if DEBUG:
+        print('DEBUG: ' + s)
+
+min_ratio = 0.75
+
+def match_song(query):
+    # remove `(xxx)`
+    query = re_brackets.sub('', query)
+    # keep only alpha
+    sig = ''.join(i for i in query if i.isalpha()).lower()
+    s = songs.get(sig)
+    if s:
+        return s
+    debugp('no direct signature match')
+    compares = []
+    for k, v in songs.items():
+        ratio = SequenceMatcher(None, k, sig).ratio()
+        compares.append((ratio, v))
+    top = sorted(compares, key=lambda x: x[0], reverse=True)[0]
+    debugp('closest match: {{}}'.format(top))
+    if top[0] > min_ratio:
+        return top[1]
+    debugp('top ratio is under {{}}, nothing really matched'.format(min_ratio))
+
+def main():
+    fmt = os.environ.get('BS_FMT', '{{vocal}} - {{year}}')
+    try:
+        query = sys.argv[1]
+    except IndexError:
+        print('Usage: beatles_searcher.py <query>')
+        sys.exit(1)
+    s = match_song(query)
+    if s:
+        print(fmt.format(**s) + '|' + s['name'])
+    else:
+        sys.exit(1)
+
+main()
+"""
 
 if __name__ == '__main__':
     main()
